edca2e705e61f48a47312383198c50a910843550
alyysaa <awokenboio@gmail.com>
2023-05-08 09:04:24 -0700

    4,readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# All Leetcode problems I have solved
+1. [Two Sum (Easy)](https://leetcode.com/problems/two-sum/) \[[Solution](https://github.com/alyysaa/leet/blob/main/TwoSum.cpp)\]
+2. [Add Two Numbers (Medium)](https://leetcode.com/problems/add-two-numbers/) \[[Solution](https://github.com/alyysaa/leet/blob/main/AddTwoNumbers.cpp)\]
+3. [Longest Substring Without Repeating Characters (Medium)](https://leetcode.com/problems/longest-substring-without-repeating-characters/) \[[Solution](https://github.com/alyysaa/leet/blob/main/LongestSubNoRepeats.cpp)\]
+4. [Median of Two Sorted Arrays (Hard)](https://leetcode.com/problems/median-of-two-sorted-arrays/) \[[Solution](https://github.com/alyysaa/leet/blob/main/SortedArrayMedian.cpp)\]
---
+++ SortedArrayMedian.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int find(vector<int> &nums1, vector<int> &nums2, int x){
+        int i = 0, j = 0, k = 1;
+        if (x==0) return x;
+        while (i<nums1.size()&&j<nums2.size()){
+            if (nums1[i]<nums2[j]){
+                if (k==x) return nums1[i];
+                i++;
+            }
+            else{
+                if (k==x) return nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<nums1.size()){
+            if (k==x) return nums1[i];
+            i++;
+            k++;
+        }
+        while (j<nums2.size()){
+            if (k==x) return nums2[j];
+            j++;
+            k++;
+        }
+        return 0;
+    }
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        int total = nums1.size() + nums2.size();
+        int x;
+        if (total&1){
+            x = total / 2 + 1;
+            return find(nums1, nums2, x);
+        }
+        x = total / 2;
+        return (find(nums1, nums2, x) + find(nums1, nums2, x + 1)) / 2.0;
+    }
+};
